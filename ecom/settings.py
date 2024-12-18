5c4e9e3ffa5b23f71da60f91b6f7cc2dff3cf5e9
Saga Charya <sagacharya77@gmail.com>
2024-12-18 21:32:45 +0545

    csrf push

--- ecom/settings.py
+++ ecom/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = 'django-insecure-gzu5j23)9t26#o88dsls0)8c9j86@!4-6qlu+yfy8_8jre7&r8
 DEBUG = True
 
 ALLOWED_HOSTS = ['django-ecom-app-production-f311.up.railway.app', 'https://django-ecom-app-production-f311.up.railway.app']
-CSRF_TRUSTED_ORIGINS = ['django-ecom-app-production-f311.up.railway.app', 'https://django-ecom-app-production-f311.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://django-ecom-app-production-f311.up.railway.app']
 
 
 # Application definition
